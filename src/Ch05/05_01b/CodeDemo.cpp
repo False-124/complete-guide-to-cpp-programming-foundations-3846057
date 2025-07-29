5c0df8d26a7310263f87b70a4175bd0f1d55f3f6
False-124 <nickolaus.r.jauregui.ctr@us.navy.mil>
2025-07-29 20:47:46 +0000

    fdh

--- src/Ch05/05_01b/CodeDemo.cpp
+++ src/Ch05/05_01b/CodeDemo.cpp
@@ -8,6 +8,27 @@ int main(){
     int score = 1023;
     bool gamePaused = false;
     char action = 'x';
+
+    if (score > 1000) {
+        std::cout << "You have a high score!" << std::endl;
+    } 
+    
+    if (score %2)
+        std::cout << "Score is odd" << std::endl;
+    else
+        std::cout << "Score is even" << std::endl;
+
+    std::cout << "The action '" << action << "' is ";
+    if (action != 'a' && action != 'w' && action != 'd' && action != 's') 
+        std::cout << "not ";
+    std::cout << "A movement key" << std::endl;
+
+    if (gamePaused) {
+        std::cout << "Game is paused" << std::endl;
+    } else {
+        std::cout << "Game is running" << std::endl;
+    }
+
     
     std::cout << std::endl << std::endl;
     return 0;
