ce0fddd69d2cc4a574324c8f230de6c422e19f36
False-124 <nickolaus.r.jauregui.ctr@us.navy.mil>
2025-07-29 17:31:12 +0000

    dgs

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -4,7 +4,22 @@
 
 #include <iostream>
 
+//#define LEVELS 4
+
 int main(){
+    const size_t LEVELS = 4; // Define the number of levels as a constant
+    int enemies[LEVELS];
+    float levelDifficulty[] = {1.0, 3.5, 6.0, 10.5};
+    enemies[0] = 10;
+    enemies[1] = 20;
+    enemies[2] = 30;
+    enemies[3] = 40;
+
+    std::cout<< "4 levels:" << std::endl;
+    std::cout<< "Level 1: " << enemies[0] << std::endl;
+    std::cout<< "Level 2: " << enemies[1] << std::endl;
+    std::cout<< "Level 3: " << enemies[2] << std::endl;
+    std::cout<< "Level 4: " << enemies[3] << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
